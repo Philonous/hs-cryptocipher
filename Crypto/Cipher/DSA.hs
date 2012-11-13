b3509a0d6ba38f116d0761c4d489db5fca769944
Philipp Balzarek <p.balzarek@googlemail.com>
2012-11-13 20:23:27 +0100

    handle failure to invert w.r.t. non-prime q in DSA module

--- Crypto/Cipher/DSA.hs
+++ Crypto/Cipher/DSA.hs
@@ -17,28 +17,31 @@ module Crypto.Cipher.DSA
 	) where
 
 import Crypto.Random
-import Data.Maybe
+import Crypto.Types.PubKey.DSA
 import Data.ByteString (ByteString)
+import Data.Maybe (isNothing)
+import Number.Generate
 import Number.ModArithmetic (exponantiation, inverse)
 import Number.Serialize
-import Number.Generate
-import Crypto.Types.PubKey.DSA
 
-data Error = 
+data Error =
 	  InvalidSignature          -- ^ signature is not valid r or s is not between the bound 0..q
 	| RandomGenFailure GenError -- ^ the random generator returns an error. give the opportunity to reseed for example.
+        | ParameterError -- ^ q is not prime. Note that a composite q does not
+                         -- necessarily lead to this error (e.g. when it's a
+                         -- Carmichael number)
 	deriving (Show,Eq)
 
 {-| sign message using the private key. -}
-sign :: CryptoRandomGen g => g -> (ByteString -> ByteString) -> PrivateKey -> ByteString -> Either GenError (Signature, g)
+sign :: CryptoRandomGen g => g -> (ByteString -> ByteString) -> PrivateKey -> ByteString -> Either Error (Signature, g)
 sign rng hash pk m =
 	-- Recalculate the signature in the unlikely case that r = 0 or s = 0
 	case generateMax rng q of
-		Left err        -> Left err
-		Right (k, rng') ->
-			let kinv = fromJust $ inverse k q in
-			let r    = expmod g k p `mod` q in
-			let s    = (kinv * (hm + x * r)) `mod` q in
+		Left err        -> Left $ RandomGenFailure err
+		Right (k, rng') -> do
+			kinv <- maybe (Left ParameterError) Right $ inverse k q
+			let r    = expmod g k p `mod` q
+			let s    = (kinv * (hm + x * r)) `mod` q
 			if r == 0 || s == 0
 				then sign rng' hash pk m
 				else Right ((r, s), rng')
@@ -52,13 +55,14 @@ verify :: Signature -> (ByteString -> ByteString) -> PublicKey -> ByteString -> 
 verify (r,s) hash pk m
 	-- Reject the signature if either 0 < r <q or 0 < s < q is not satisfied.
 	| r <= 0 || r >= q || s <= 0 || s >= q = Left InvalidSignature
+        | isNothing w'                         = Left ParameterError
 	| otherwise                            = Right $ v == r
 	where
 		(p,g,q) = public_params pk
 		y       = public_y pk
 		hm      = os2ip $ hash m
-
-		w       = fromJust $ inverse s q
+		w'      = inverse s q
+		w       = maybe (error "DSA.verify: inverse is Nothing") id w'
 		u1      = (hm*w) `mod` q
 		u2      = (r*w) `mod` q
 		v       = ((expmod g u1 p) * (expmod y u2 p)) `mod` p `mod` q
